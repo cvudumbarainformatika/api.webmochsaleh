c8b438495c08ca7273a6341fb0fd8b9605812894
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2022-12-07 09:09:17 +0700

    ppid done

--- routes/v1/ppid.php
+++ routes/v1/ppid.php
@@ -10,14 +10,14 @@ use Illuminate\Support\Facades\Route;
 
 
 // Route::get('/berita/data_beranda', [BeritaController::class, 'web_beranda']);
-Route::get('/pelayanan/web_content', [PpidController::class, 'web_content']);
+Route::get('/ppid/web_content', [PpidController::class, 'web_content']);
 // Route::get('/berita/kota', [ScrapperController::class, 'index']);
 
 Route::middleware('auth:api')
     ->group(function () {
-        Route::get('/pelayanans', [PpidController::class, 'index']);
-        Route::post('/pelayanan/store', [PpidController::class, 'store']);
-        Route::post('/pelayanan/destroy', [PpidController::class, 'destroy']);
+        Route::get('/ppids', [PpidController::class, 'index']);
+        Route::post('/ppid/store', [PpidController::class, 'store']);
+        Route::post('/ppid/destroy', [PpidController::class, 'destroy']);
         // Route::post('/berita/upload_word', [UploadWord::class, 'upload_word']);
         // Route::post('/berita/update_status', [BeritaController::class, 'update_status']);
     });
